18ad96eea5a8e4275b7770291e14088c7cc0983f
Yukichi <naginoyukichi@gmail.com>
2022-11-08 09:21:57 +0900

    python アプリ

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+# 画像読み込み
+from PIL import Image
 
 st.title('streamlit 超入門')
 
@@ -15,7 +17,6 @@ df = pd.DataFrame(
 st.line_chart(df)
 # 折線エリア塗り
 st.area_chart(df)
-
 
 # データフレームの表示について
 # st.dataframe(df, width=100, height=100)
@@ -39,3 +40,31 @@ import pandas as pd
 ```
 
 """
+
+# map
+df = pd.DataFrame(
+  np.random.rand(100, 2) + [35, 139],
+  columns=['lat', 'lon']
+)
+st.map(df)
+
+st.write('Display Image')
+
+condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+'コンディション : ', condition
+
+# テキスト入力
+text = st.text_input('あなたの趣味を教えてください')
+'あなたの趣味 : ', text
+
+# 指定した値を表示
+option = st.selectbox(
+  'あなたが好きな数字を教えてください',
+  list(range(1, 11))
+)
+'あなたが好きな数字は、', option, 'です。'
+
+# チェックボックスで画像表示
+if st.checkbox('Show Image'):
+  img = Image.open('sample.jpg')
+  st.image(img, caption='nagino', use_column_width=True)
