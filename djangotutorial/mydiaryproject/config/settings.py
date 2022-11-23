704cffafffe20e2db47706b18d38ed26deceb5fe
Yukichi <naginoyukichi@gmail.com>
2022-11-23 10:53:42 +0900

    python app

--- djangotutorial/mydiaryproject/config/settings.py
+++ djangotutorial/mydiaryproject/config/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'ja-jp'
 
 TIME_ZONE = 'Asia/Tokyo'
 
